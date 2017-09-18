7585e8aebbc83c7651b8342922b8144e13ddd2c5
David <davidcoates95@gmail.com>
2017-09-18 13:51:17 +1000

    details grouped by instance size

--- benchmark.sh
+++ benchmark.sh
@@ -13,18 +13,40 @@ if [ ! -d $BENCHMARK_DIR ]; then
  cd ../
 fi
 
-n=$(ls -1q $BENCHMARK_DIR | wc -l)
+SUITES=(20 50 75 100)
 
-benchmark () {
+benchmark() {
+  echo "$@"
+  echo ""  
+  total_timeout=0
+  total_timein=0
+  total_total=0
+  for s in "${SUITES[@]}"; do
+    echo "$s variables"
+    suite_benchmark $s $@
+    total_timein=$((total_timein + timein))
+    total_timeout=$((total_timeout + timeout))
+    total_total=$(echo $total_total + $total | bc -l)
+  done
+  echo "### TOTALS ###"
+  average=$(echo $total_total / $total_timein | bc -l)
+  average=$(printf "%.3f" $average)
+  echo "Instances solved [$total_timein] - Timed-out [$total_timeout] - Average time(s) [$average]"
+  echo ""
+}
+
+suite_benchmark () {
+ suite=$1
+ shift
  timeout=0
  timein=0
  total=0
+ n=$(ls -1q $BENCHMARK_DIR/uf$suite-*.cnf | wc -l)
  remaining=$n
  percent=0
- echo "$1"
  echo -ne "Instances solved [$timein] - Timed-out [$timeout] - Remaining [$remaining] ($percent%)         \r"
- for f in $BENCHMARK_DIR/*.cnf; do
-   if output=$( (/usr/bin/time --quiet -f "%U" python3 solver.py $1 $f > /dev/null) 2>&1); then
+ for f in $BENCHMARK_DIR/uf$suite-*.cnf; do
+   if output=$( (/usr/bin/time --quiet -f "%U" python3 solver.py $@ $f > /dev/null) 2>&1); then
      total=$(echo $total + $output | bc -l)
      timein=$((timein+1))
    else
@@ -36,16 +58,26 @@ benchmark () {
  done
  echo ""
  average=$(echo $total / $timein | bc -l)
- average=$(printf "%.2f" $average)
+ average=$(printf "%.3f" $average)
  echo "Average time(s) [$average]"
- echo ""
 }
 
 
-echo "Benchmarking on $n instances..."
+echo "Exhaustive UP & PL"
+benchmark "--choose-type=dlis --UP-interval=1 --PL-interval=1"
 
-echo "Variable selection: DLIS"
-benchmark "--choose-type=dlis"
+echo "UP & PL every 2nd level"
+benchmark "--choose-type=dlis --UP-interval=2 --PL-interval=2"
 
-echo "Variable selection: Jeroslow-Wang"
-benchmark "--choose-type=jw"
+echo "UP & PL every 3rd level"
+benchmark "--choose-type=dlis --UP-interval=3 --PL-interval=3"
+
+echo "Only PL"
+benchmark "--choose-type=dlis --UP-interval=0 --PL-interval=1"
+
+echo "Only UP"
+benchmark "--choose-type=dlis --UP-interval=1 --PL-interval=0"
+
+echo "No UP, No PL"
+benchmark "--choose-type=dlis --UP-interval=0 --PL-interval=0"
+

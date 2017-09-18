4c9676383f6008b6f3c5918566cc4f9d40f0fc12
David <davidcoates95@gmail.com>
2017-09-18 12:14:46 +1000

    update benchmark instances

--- benchmark.sh
+++ benchmark.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+
+
 BENCHMARK_DIR="benchmark-instances"
 if [ ! -d $BENCHMARK_DIR ]; then
   echo "#-- Downloading benchmark instances --#"
   mkdir $BENCHMARK_DIR
   cd $BENCHMARK_DIR
-  wget http://www.cs.ubc.ca/~hoos/SATLIB/Benchmarks/SAT/RND3SAT/uf100-430.tar.gz
+  wget http://www.cse.unsw.edu.au/~dcoa057/benchmark-instances.tar.gz
   tar -xzf *.tar.gz
+  rm *.tar.gz
   cd ../
 fi
 

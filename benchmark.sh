12ec16f0dd70163828cf43422693307410608a88
David <davidcoates95@gmail.com>
2017-09-18 13:18:46 +1000

    fix portability issues

--- benchmark.sh
+++ benchmark.sh
@@ -4,40 +4,41 @@
 
 BENCHMARK_DIR="benchmark-instances"
 if [ ! -d $BENCHMARK_DIR ]; then
-  echo "#-- Downloading benchmark instances --#"
-  mkdir $BENCHMARK_DIR
-  cd $BENCHMARK_DIR
-  wget http://www.cse.unsw.edu.au/~dcoa057/benchmark-instances.tar.gz
-  tar -xzf *.tar.gz
-  rm *.tar.gz
-  cd ../
+ echo "#-- Downloading benchmark instances --#"
+ mkdir $BENCHMARK_DIR
+ cd $BENCHMARK_DIR
+ wget http://www.cse.unsw.edu.au/~dcoa057/benchmark-instances.tar.gz
+ tar -xzf *.tar.gz
+ rm *.tar.gz
+ cd ../
 fi
 
 n=$(ls -1q $BENCHMARK_DIR | wc -l)
 
-function benchmark {
-  timeout=0
-  timein=0
-  total=0
-  remaining=$n
-  percent=0
-  echo -ne "Instances solved [$timein] - Timed-out [$timeout] - Remaining [$remaining] ($percent%)         \r"
-  for f in $BENCHMARK_DIR/*.cnf; do
-    if output=$((/usr/bin/time --quiet -f "%U" python3 solver.py $1 $f > /dev/null) 2>&1); then
-      total=$(echo $total + $output | bc -l)
-      timein=$((timein+1))
-    else
-      timeout=$((timeout+1))
-    fi
-    remaining=$((remaining-1))
-    percent=$((100 - (100*remaining/n)))
-    echo -ne "Instances solved [$timein], Timed-out [$timeout], Remaining [$remaining] ($percent%)        \r"
-  done
-  echo ""
-  average=$(echo $total / $timein | bc -l)
-  average=$(printf "%.2f" $average)
-  echo "Average time(s) [$average]"
-  echo ""
+benchmark () {
+ timeout=0
+ timein=0
+ total=0
+ remaining=$n
+ percent=0
+ echo "$1"
+ echo -ne "Instances solved [$timein] - Timed-out [$timeout] - Remaining [$remaining] ($percent%)         \r"
+ for f in $BENCHMARK_DIR/*.cnf; do
+   if output=$( (/usr/bin/time --quiet -f "%U" python3 solver.py $1 $f > /dev/null) 2>&1); then
+     total=$(echo $total + $output | bc -l)
+     timein=$((timein+1))
+   else
+     timeout=$((timeout+1))
+   fi
+   remaining=$((remaining-1))
+   percent=$((100 - (100*remaining/n)))
+   echo -ne "Instances solved [$timein], Timed-out [$timeout], Remaining [$remaining] ($percent%)        \r"
+ done
+ echo ""
+ average=$(echo $total / $timein | bc -l)
+ average=$(printf "%.2f" $average)
+ echo "Average time(s) [$average]"
+ echo ""
 }
 
 
@@ -48,4 +49,3 @@ benchmark "--choose-type=dlis"
 
 echo "Variable selection: Jeroslow-Wang"
 benchmark "--choose-type=jw"
-
